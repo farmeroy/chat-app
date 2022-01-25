cf72b406a2bd8301d2df29d6e036958c026e9e0a
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-25 13:45:39 +0100

    refactored name, nav...setOptions inside componentDidMount (error related to state rendering)

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 import { View, Text, Button } from "react-native";
 
 class Chat extends Component {
+
+  componentDidMount() {
+
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({title: name});
+    } 
   render() {
+    
+
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Hello Screen1!</Text>
