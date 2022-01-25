52f4c9cd2bbb87bbaba8a7c0f6e251128ab1f6ef
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-25 10:46:16 +0100

    removed nested navigation, prettier

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler";
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Chat from "./components/Chat";
+import Start from "./components/Start";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Start">
+            <Stack.Screen name="Start" component={Start} />
+            <Stack.Screen name="Chat" component={Chat} />
+          </Stack.Navigator>
+        </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
