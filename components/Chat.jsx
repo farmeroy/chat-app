69f5e239affc0dba1a3b9a888421e7743dfacfc3
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-25 10:46:56 +0100

    removed Button

---
+++ components/Chat.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text, Button } from "react-native";
+
+class Chat extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Hello Screen1!</Text>
+      </View>
+    );
+  }
+}
+
+export default Chat;
