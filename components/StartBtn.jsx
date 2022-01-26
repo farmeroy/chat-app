45304b6597779e276544291d677fcc194d04cfbf
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-26 13:18:50 +0100

    centered text, added height, text from props

---
+++ components/StartBtn.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {Pressable, View, Text, StyleSheet} from 'react-native';
+
+export default class StartBtn extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <Pressable onPress={this.props.onPress} style={styles.startBtn}>
+        <View style={styles.container}>
+        <Text style={styles.text}>{this.props.title}</Text>
+        </View>
+      </Pressable>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  startBtn: {
+    width: '88%',
+    height: 60,
+    backgroundColor: "#757083",
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#fff',
+    alignSelf: 'center'
+  },
+  container: {
+    flex:1,
+    justifyContent: 'center'
+  }
+})
