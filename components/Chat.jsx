bacb5ba7c123f4ead57b9d6990a6e53188612e7d
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-26 11:44:03 +0100

    style background color from start state chatColor

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import { View, Text, Button } from "react-native";
 
 class Chat extends Component {
-
   componentDidMount() {
-
     let name = this.props.route.params.name;
-    this.props.navigation.setOptions({title: name});
-    } 
+    this.props.navigation.setOptions({ title: name });
+  }
   render() {
-    
-
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View
+        style={{
+          backgroundColor: this.props.route.params.chatColor,
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Text>Hello Screen1!</Text>
       </View>
     );
