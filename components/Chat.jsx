86e31bf0686f7ba8d9cc47cf6bc67ecb594167f5
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-01-27 14:48:04 +0100

    added render methods and custom components for Day, Bubble, and SystemMessage

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,40 +1,85 @@
 import React, { Component } from "react";
-import { GiftedChat } from 'react-native-gifted-chat';
-import { View, Text, Button } from "react-native";
+import { Day, SystemMessage, Bubble, GiftedChat } from "react-native-gifted-chat";
+import { View, Platform, KeyboardAvoidingView } from "react-native";
 
 class Chat extends Component {
   constructor() {
     super();
     this.state = {
       messages: [],
-    }
+    };
   }
   componentDidMount() {
-    // set our message state here 
+    // set our message state here
     // this will be an AJAX request to firebase,
     // best practice to perform in componentDidMount
     this.setState({
       messages: [
         {
           _id: 1,
-          text: 'Hello developer',
+          text: "Hello developer",
           createdAt: new Date(),
           user: {
             _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          }
-        }
-      ]
-    })
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: "This is a system message",
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
   }
 
   onSend(messages = []) {
     this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages)
+      messages: GiftedChat.append(previousState.messages, messages),
     }));
+  }
+
+  // custom setting for the <Day/> message
+  renderDay(props) {
+    return (
+      <Day
+        {...props}
+        textStyle={{
+          color: "#fff",
+        }}
+      />
+    );
+  }
+
+  // change the styles for system messages
+  renderSystemMessage(props) {
+    return (
+      <SystemMessage
+        {...props}
+        textStyle={{
+          color: "#fff",
+        }}
+      />
+    );
+  }
+
+  // function that allows custom Bubble settings
+  // target each wrapp with either right or left
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#000",
+          },
+        }}
+      />
+    );
   }
 
   render() {
@@ -43,16 +88,23 @@ class Chat extends Component {
         style={{
           backgroundColor: this.props.route.params.chatColor,
           flex: 1,
-          flexDirection: 'column',
+          flexDirection: "column",
         }}
       >
         <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          renderDay={this.renderDay.bind(this)}
+          renderSystemMessage={this.renderSystemMessage.bind(this)}
           messages={this.state.messages}
-          onSend={messages => this.onSend(messages)}
+          onSend={(messages) => this.onSend(messages)}
           user={{
             _id: 1,
           }}
         />
+        {/* fix for older android systems */}
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
     );
   }
